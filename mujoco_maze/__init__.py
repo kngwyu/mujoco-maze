27ebe4ffc15688d9af7d12843451df2f3e0dbeaa
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-05-29 16:44:27 +0900

    manual_collision=True

--- mujoco_maze/__init__.py
+++ mujoco_maze/__init__.py
@@ -7,7 +7,7 @@ for maze_id in MAZE_IDS:
     gym.envs.register(
         id="AntMaze{}-v0".format(maze_id),
         entry_point="mujoco_maze.ant_maze_env:AntMazeEnv",
-        kwargs=dict(maze_id=maze_id),
+        kwargs=dict(maze_id=maze_id, manual_collision=True),
         max_episode_steps=1000,
         reward_threshold=-1000,
     )
@@ -16,7 +16,7 @@ for maze_id in MAZE_IDS:
     gym.envs.register(
         id="PointMaze{}-v0".format(maze_id),
         entry_point="mujoco_maze.point_maze_env:PointMazeEnv",
-        kwargs=dict(maze_id=maze_id),
+        kwargs=dict(maze_id=maze_id, manual_collision=True),
         max_episode_steps=1000,
         reward_threshold=-1000,
     )
