c91a4bc8a75cb671349fc48685415ebc3e984457
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-06-04 14:38:15 +0900

    More detailed tests

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -7,9 +7,13 @@ import pytest
 def test_ant_maze(maze_id):
     env = gym.make("Ant{}-v0".format(maze_id))
     assert env.reset().shape == (30,)
+    s, _, _, _ = env.step(env.action_space.sample())
+    assert s.shape == (30,)
 
 
 @pytest.mark.parametrize("maze_id", mujoco_maze.MAZE_IDS)
 def test_point_maze(maze_id):
     env = gym.make("Point{}-v0".format(maze_id))
     assert env.reset().shape == (7,)
+    s, _, _, _ = env.step(env.action_space.sample())
+    assert s.shape == (7,)
